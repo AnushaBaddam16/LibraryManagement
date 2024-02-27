89ba8312dc6490d56ca49494aaeefc8702898388
anushabaddam16 <anureddyb.work@gmail.com>
2024-02-27 17:37:33 -0600

    added

--- src/main/java/com/Library/demo/LibraryServiceApplication.java
+++ src/main/java/com/Library/demo/LibraryServiceApplication.java
@@ -2,8 +2,11 @@ package com.Library.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class LibraryServiceApplication {
 
     public static void main(String[] args) {
@@ -11,3 +14,9 @@ public class LibraryServiceApplication {
     }
 
 }
+
+
+
+
+
+
